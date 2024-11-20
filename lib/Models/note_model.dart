cbf137261db385873986c61b435d0d3c2585dc04
reem <reemmohmedmohmed@gmail.com>
2024-11-20 11:36:33 +0200

    creat note Model

---
+++ lib/Models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String titel;
+  final String subtitel;
+  final String date;
+  final int color;
+
+  NoteModel(
+      {required this.titel,
+      required this.subtitel,
+      required this.date,
+      required this.color});
+}
