6b67e431de4f3472329211914a15ed39cbdf8700
reem <reemmohmedmohmed@gmail.com>
2024-11-21 12:33:46 +0200

    int hive

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kprimarycolor = Color(0xff53ebd6);
+const kNotesBox = 'notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notsapp/constant.dart';
 import 'package:notsapp/views/widgets/notes_view_body.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const NotApp());
 }
 
